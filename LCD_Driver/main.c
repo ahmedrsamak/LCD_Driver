dc51bbf7791d950f7128a2dcfc0d9dfd32f310f3
ahmed samak <41822278+ahmedrsamak@users.noreply.github.com>
2018-07-29 00:39:31 +0200

    Add files via upload

---
+++ LCD_Driver/main.c
@@ -0,0 +1,18 @@
+/*
+ * LCD_Driver.c
+ *
+ * Created: 7/28/2018 12:39:12 PM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */ //
+    while (1) 
+    {
+    }
+}
+
