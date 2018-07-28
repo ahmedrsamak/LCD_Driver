13721393889b541311cabf4b67add94bd1c25d97
ahmed <ahmedrsamak@gmail.com>
2018-07-29 01:17:10 +0200

    add common.h

---
+++ LCD_Driver/common.h
@@ -0,0 +1,18 @@
+/*
+ * common.h
+ *
+ * Created: 7/24/2018 12:12:58 AM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef COMMON_H_
+#define COMMON_H_
+#define NULL ((void *)0)
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+
+
+
+#endif /* COMMON_H_ */
